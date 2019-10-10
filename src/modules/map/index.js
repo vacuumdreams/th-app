15baed1066dfcf8e4482aff8255c0a4d9ab20780
Botond Fekete <fekbot@gmail.com>
2019-10-10 00:05:33 -0600

    Fix lint

--- src/modules/map/index.js
+++ src/modules/map/index.js
@@ -8,6 +8,17 @@ import "./index.scss"
 
 const token = process.env.GATSBY_MAPBOX_ACCESS_TOKEN
 
+function GeocoderPlaceholder (props) {
+  return (
+    <div className="mapboxgl-ctrl-geocoder mapboxgl-ctrl">
+      <svg className="mapboxgl-ctrl-geocoder--icon mapboxgl-ctrl-geocoder--icon-search" viewBox="0 0 18 18" width="18" height="18">
+        <path d="M7.4 2.5c-2.7 0-4.9 2.2-4.9 4.9s2.2 4.9 4.9 4.9c1 0 1.8-.2 2.5-.8l3.7 3.7c.2.2.4.3.8.3.7 0 1.1-.4 1.1-1.1 0-.3-.1-.5-.3-.8L11.4 10c.4-.8.8-1.6.8-2.5.1-2.8-2.1-5-4.8-5zm0 1.6c1.8 0 3.2 1.4 3.2 3.2s-1.4 3.2-3.2 3.2-3.3-1.3-3.3-3.1 1.4-3.3 3.3-3.3z" />
+      </svg>
+      <input type="text" className="mapboxgl-ctrl-geocoder--input" placeholder={props.placeholder} />
+    </div>
+  )
+}
+
 class SearchableMap extends Component {
   state = {
     searched: false,
@@ -18,7 +29,7 @@ class SearchableMap extends Component {
     },
     searchResultLayer: null,
     MapGL: null,
-    Geocoder: null,
+    Geocoder: GeocoderPlaceholder,
     GeoJsonLayer: null,
   }
 
@@ -75,7 +86,6 @@ class SearchableMap extends Component {
         viewport,
         MapGL,
         Geocoder,
-        GeoJsonLayer,
       } = this.state
 
       return (
@@ -113,7 +123,6 @@ class SearchableMap extends Component {
                     containerRef={this.containerRef}
                     placeholder="Search for places"
                     render={(props) => {
-                      console.log(props)
                       return (`
                         <div>
                           ${props.text}
