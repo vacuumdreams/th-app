0942013a74d83352ca77f8ed735202bc535b46ef
Botond Fekete <fekbot@gmail.com>
2019-11-24 22:02:24 +0700

    Try external grpc

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,5 @@
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
-    node: {
-      fs: 'empty',
-      child_process : 'empty',
-      net : 'empty',
-      tls: 'empty',
-    },
     externals: ['grpc'],
     resolve: {
       mainFields: ['main'],
