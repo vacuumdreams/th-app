fabc00a7356aaa337a1af9639bc108889430871e
Botond Fekete <fekbot@gmail.com>
2019-11-26 10:16:34 +0700

    Add temporary landing page

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,16 +28,18 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png',
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-fonts',
+      options: {
+        fonts: [
+          'material icons',
+          'montserrat:300',
+          'gloria hallelujah:400',
+        ]
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
-    {
-      resolve: 'gatsby-source-sanity',
-      options: {
-        projectId: 'ocnnw83z',
-        dataset: 'production',
-        token: process.env.SANITY_PRIVATE_TOKEN,
-      },
-    }
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,21 @@
 import React from "react"
 // import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-// import Image from "../components/image"
 import SEO from "../components/seo"
-import "../theme/index.scss"
+import './theme.css'
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <section className="hero is-dark is-fullheight">
-      <div style={{ maxWidth: '700px', width: '70%' }}>
-        <h1 className="title has-text-weight-bold is-size-1-tablet is-size-2-mobile">
-          Life in plastic, it's <span className="is-family-secondary">NOT</span> fantastic.
-        </h1>
+  <div className="main">
+    <SEO title="Trashhold" />
+    <section>
+      <div className="wrap">
+        <div className="inner">
+          <h1>trashhold</h1>
+          <p className="sub">coming soon</p>
+        </div>
       </div>
     </section>
-  </Layout>
+  </div>
 )
 
 export default IndexPage
